16197452d8abd69b99d7d054c94ce8830022fb0b
Myrzabek <myrzabekn911@gmail.com>
2018-11-25 12:53:21 +0600

    first change in my repository

---
+++ index.php
@@ -0,0 +1,280 @@
+<?php get_header(); ?>
+
+   <!-- Intro Section
+   ================================================== -->
+   <section id="intro">
+
+      <!-- Flexslider Start-->
+	   <div id="intro-slider" class="flexslider">
+
+		   <ul class="slides">
+
+			   <!-- Slide -->
+			   <li>
+				   <div class="row">
+					   <div class="twelve columns">
+						   <div class="slider-text">
+							   <h1>Lorem ipsum dolor sit<span>.</span></h1>
+							   <p>Aenean condimentum, lacus sit amet luctus lobortis, dolores et quas molestias excepturi
+                        enim tellus ultrices elit, amet consequat enim elit noneas sit amet luctu. lacus sit amet luctus lobortis, dolores et quas molestias excepturi
+                        enim tellus ultrices elit.</p>
+						   </div>
+                     <div class="slider-image">
+                        <img src="images/sliders/home-slider-image-01.png" alt="" />
+                     </div>
+					   </div>
+				   </div>
+			   </li>
+
+            <!-- Slide -->
+			   <li>
+				   <div class="row">
+					   <div class="twelve columns">
+						   <div class="slider-text">
+							   <h1>Lorem ipsum dolor sit<span>.</span></h1>
+							   <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum
+                        deleniti eos et accusamus. amet consequat enim elit noneas sit amet luctu. lacus sit amet luctus lobortis.
+                        Aenean condimentum, lacus sit amet luctus.</p>
+						   </div>
+                     <div class="slider-image">
+                        <img src="images/sliders/home-slider-image-02.png" alt="" />
+                     </div>
+					   </div>
+				   </div>
+			   </li>
+
+		   </ul>
+
+	   </div> <!-- Flexslider End-->
+
+   </section> <!-- Intro Section End-->
+
+   <!-- Info Section
+   ================================================== -->
+   <section id="info">
+
+      <div class="row">
+
+         <div class="bgrid-quarters s-bgrid-halves">
+
+           <div class="columns">
+              <h2>Lorem ipsum.</h2>
+
+              <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+              Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+              </p>
+           </div>
+
+           <div class="columns">
+              <h2>Lorem ipsum.</h2>
+
+              <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+              Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+              </p>
+           </div>
+
+           <div class="columns s-first">
+              <h2>Lorem ipsum.</h2>
+
+              <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+              Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+              </p>
+           </div>
+
+           <div class="columns">
+              <h2>Lorem ipsum.</h2>
+
+              <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+              Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+              </p>
+           </div>
+
+           </div>
+
+      </div>
+
+   </section> <!-- Info Section End-->
+
+   <!-- Works Section
+   ================================================== -->
+   <section id="works">
+
+      <div class="row">
+
+         <div class="twelve columns align-center">
+            <h1>Lorem ipsum dolor sit amet.</h1>
+         </div>
+
+         <div id="portfolio-wrapper" class="bgrid-quarters s-bgrid-halves">
+
+    		   <div class="columns portfolio-item">
+               <div class="item-wrap">
+    				   <a href="portfolio.html">
+                     <img alt="" src="images/portfolio/geometrics.jpg">
+                     <div class="overlay"></div>
+                     <div class="link-icon"><i class="fa fa-link"></i></div>
+                  </a>
+    					<div class="portfolio-item-meta">
+    					   <h5><a href="portfolio.html">Lorem ipsum.</a></h5>
+                     <p>Lorem ipsum.</p>
+    					</div>
+               </div>
+    			</div>
+
+            <div class="columns portfolio-item">
+               <div class="item-wrap">
+    				   <a href="portfolio.html">
+                     <img alt="" src="images/portfolio/console.jpg">
+                     <div class="overlay"></div>
+                     <div class="link-icon"><i class="fa fa-link"></i></div>
+                  </a>
+    					<div class="portfolio-item-meta">
+    					   <h5><a href="portfolio.html">Lorem ipsum.</a></h5>
+                     <p>Lorem ipsum.</p>
+    					</div>
+               </div>
+    			</div>
+
+            <div class="columns portfolio-item s-first">
+               <div class="item-wrap">
+    				   <a href="portfolio.html">
+                     <img alt="" src="images/portfolio/camera-man.jpg">
+                     <div class="overlay"></div>
+                     <div class="link-icon"><i class="fa fa-link"></i></div>
+                  </a>
+    					<div class="portfolio-item-meta">
+    					   <h5><a href="portfolio.html">Lorem ipsum.</a></h5>
+                     <p>Lorem ipsum.</p>
+    					</div>
+               </div>
+    			</div>
+
+            <div class="columns portfolio-item">
+               <div class="item-wrap">
+    				   <a href="portfolio.html">
+                     <img alt="" src="images/portfolio/into-the-light.jpg">
+                     <div class="overlay"></div>
+                     <div class="link-icon"><i class="fa fa-link"></i></div>
+                  </a>
+    					<div class="portfolio-item-meta">
+    					   <h5><a href="portfolio.html">Lorem ipsum.</a></h5>
+                     <p>Lorem ipsum.</p>
+    					</div>
+               </div>
+    			</div>
+
+         </div>
+
+      </div>
+
+   </section> <!-- Works Section End-->
+
+   <!-- Journal Section
+   ================================================== -->
+   <section id="journal">
+
+      <div class="row">
+         <div class="twelve columns align-center">
+            <h1>Our latest posts and rants.</h1>
+         </div>
+      </div>
+
+      <div class="blog-entries">
+
+         <!-- Entry -->
+         <article class="row entry">
+
+            <div class="entry-header">
+
+               <div class="permalink">
+                  <a href="single.html"><i class="fa fa-link"></i></a>
+               </div>
+
+               <div class="ten columns entry-title pull-right">
+                  <h3><a href="single.html">Proin gravida nibh vel velit auctor aliquet Aenean sollicitudin auctor.</a></h3>
+               </div>
+
+               <div class="two columns post-meta end">
+                  <p>
+                  <time datetime="2014-01-31" class="post-date" pubdate="">Dec 21, 2018</time>
+                  <span class="dauthor">By Myrzabek Azil</span>
+                  </p>
+               </div>
+
+            </div>
+
+            <div class="ten columns offset-2 post-content">
+               <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum
+               deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate.
+               At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium.
+               <a class="more-link" href="single.html">Read More<i class="fa fa-arrow-circle-o-right"></i></a></p>
+            </div>
+
+         </article> <!-- Entry End -->
+
+         <!-- Entry -->
+         <article class="row entry">
+
+            <div class="entry-header">
+
+               <div class="permalink">
+                  <a href="single.html"><i class="fa fa-link"></i></a>
+               </div>
+
+               <div class="ten columns entry-title pull-right">
+                  <h3><a href="single.html">Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit sed.</a></h3>
+               </div>
+
+               <div class="two columns post-meta end">
+                  <p>
+                  <time datetime="2014-01-29" class="post-date" pubdate="">Dec 21, 2018</time>
+                  <span class="dauthor">By Myrzabek Azil</span>
+                  </p>
+               </div>
+
+            </div>
+
+            <div class="ten columns offset-2 post-content">
+               <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum
+               deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate.
+               At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium.
+               <a class="more-link" href="single.html">Read More<i class="fa fa-arrow-circle-o-right"></i></a></p>
+            </div>
+
+         </article> <!-- Entry End -->
+
+         <!-- Entry -->
+         <article class="row entry">
+
+            <div class="entry-header">
+
+               <div class="permalink">
+                  <a href="single.html"><i class="fa fa-link"></i></a>
+               </div>
+
+               <div class="ten columns entry-title pull-right">
+                  <h3><a href="blog-single.html">Quis autem vel esse eum iure reprehenderit qui in ea voluptate velit esse.</a></h3>
+               </div>
+
+               <div class="two columns post-meta end">
+                  <p>
+                  <time datetime="2014-01-28" class="post-date" pubdate="">Dec 21, 2018</time>
+                  <span class="dauthor">By Myrzabek Azil</span>
+                  </p>
+               </div>
+
+            </div>
+
+            <div class="ten columns offset-2 post-content">
+               <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum
+               deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate.
+               At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium.
+               <a class="more-link" href="single.html">Read More<i class="fa fa-arrow-circle-o-right"></i></a></p>
+            </div>
+
+         </article> <!-- Entry End -->
+
+      </div> <!-- Entries End -->
+
+   </section> <!-- Journal Section End-->
+<?php get_footer(); ?>
---
+++ sidebar.php
@@ -0,0 +1,58 @@
+            <aside id="sidebar">
+
+               <div class="widget widget_search">
+                  <h5>Search</h5>
+                  <form action="#">
+
+                     <input class="text-search" type="text" onfocus="if (this.value == 'Search here...') { this.value = ''; }" onblur="if(this.value == '') { this.value = 'Search here...'; }" value="Search here...">
+                     <input type="submit" class="submit-search" value="">
+
+                  </form>
+               </div>
+
+               <div class="widget widget_text">
+                  <h5 class="widget-title">Text Widget</h5>
+                  <div class="textwidget">Proin gravida nibh vel velit auctor aliquet.
+                  Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum,
+                  nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus
+                  a sit amet mauris. Morbi accumsan ipsum velit. </div>
+		         </div>
+
+               <div class="widget widget_categories">
+                  <h5 class="widget-title">Categories</h5>
+                  <ul class="link-list cf">
+                     <li><a href="#">Categories first</a></li>
+                     <li><a href="#">Categories second</a></li>
+                     <li><a href="#">Categories third</a></li>
+                     <li><a href="#">Categories four</a></li>
+                     <li><a href="#">Categories five</a></li>
+                     <li><a href="#">Categories six</a></li>
+                     <li><a href="#">Categories seven</a></li>
+                  </ul>
+               </div>
+
+               <div class="widget widget_tag_cloud">
+                  <h5 class="widget-title">Tags</h5>
+                  <div class="tagcloud cf">
+                     <a href="#">Tags first</a>
+                     <a href="#">Tags second</a>
+                     <a href="#">Tags third</a>
+                     <a href="#">Tags four</a>
+                  </div>
+               </div>
+
+               <div class="widget widget_photostream">
+                  <h5>Photostream</h5>
+                  <ul class="photostream cf">
+                     <li><a href="#"><img src="images/thumb.jpg" alt="thumbnail"></a></li>
+                     <li><a href="#"><img src="images/thumb.jpg" alt="thumbnail"></a></li>
+                     <li><a href="#"><img src="images/thumb.jpg" alt="thumbnail"></a></li>
+                     <li><a href="#"><img src="images/thumb.jpg" alt="thumbnail"></a></li>
+                     <li><a href="#"><img src="images/thumb.jpg" alt="thumbnail"></a></li>
+                     <li><a href="#"><img src="images/thumb.jpg" alt="thumbnail"></a></li>
+                     <li><a href="#"><img src="images/thumb.jpg" alt="thumbnail"></a></li>
+                     <li><a href="#"><img src="images/thumb.jpg" alt="thumbnail"></a></li>
+                  </ul>
+	            </div>
+
+            </aside>
